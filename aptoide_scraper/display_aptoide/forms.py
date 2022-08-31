7ffb122a8393658bdf614d27f6f5d73d9623c9d4
Tom <tomrfreeman3@gmail.com>
2022-08-31 21:53:26 +0100

    initial run: webscraper + flask app

--- aptoide_scraper/display_aptoide/forms.py
+++
@@ -1,8 +0,0 @@
-# listings/forms.py
-
-from django import forms
-
-class ContactUsForm(forms.Form):
-   url = forms.CharField(required=True)
-   email = forms.EmailField()
-   message = forms.CharField(max_length=1000)
